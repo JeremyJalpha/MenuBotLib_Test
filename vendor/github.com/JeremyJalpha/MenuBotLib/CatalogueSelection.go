86b3beb85d21e49a5fc405b3c9d3af46483e606a
JeremyJalpha <jeremyjalpha@gmail.com>
2024-07-11 20:02:22 +0200

    Added PrlstPreamble to ConversationContext, removed unnecessary catalogueID param from UpdateOrInsertCurrentOrder, Fixed params in method calls in whatsappbot.go, renamed static_pricelist to CatalogueSelection

---
+++ vendor/github.com/JeremyJalpha/MenuBotLib/CatalogueSelection.go
@@ -0,0 +1,49 @@
+package menubotlib
+
+import (
+	"fmt"
+)
+
+// CatalogueSelection represents a section of the catalogue with a specific pricing regime
+type CatalogueSelection struct {
+	Preamble string
+	Items    []CatalogueItem
+}
+
+// Generate a string for a single question and answer
+func CatalogueItemAsAString(item CatalogueItem) string {
+	optionsText := ""
+	for i, option := range item.Options {
+		optionsText += fmt.Sprintf("   %d. %s\n", i+1, option)
+	}
+
+	qA := fmt.Sprintf("%d: %s\n%s\n", item.CatalogueItemID, item.Item, optionsText)
+
+	return qA
+}
+
+// Iterate over Questions array and populate questions array
+func SingleSelectionAsAString(section CatalogueSelection) string {
+	allItems := section.Preamble + "\n"
+
+	for _, item := range section.Items {
+		allItems += CatalogueItemAsAString(item)
+	}
+
+	return allItems
+}
+
+// Updated function to use the new CatalogueSection structure
+func AssembleCatalogueSelections(pricelistpreamble string, selections []CatalogueSelection) string {
+	selectionString := pricelistpreamble + "\n\n"
+
+	for i, selection := range selections {
+		// Outdoor Selection
+		selectionString += SingleSelectionAsAString(selection)
+		if i < len(selections)-1 {
+			selectionString += "\n"
+		}
+	}
+
+	return selectionString
+}
---
+++ vendor/github.com/JeremyJalpha/MenuBotLib/conversationcontext.go
@@ -0,0 +1,29 @@
+package menubotlib
+
+import (
+	"database/sql"
+	"time"
+)
+
+type ConversationContext struct {
+	UserInfo      UserInfo
+	UserExisted   bool
+	PrlstPreamble string
+	PriceList     []CatalogueSelection
+	CurrentOrder  CustomerOrder
+	MessageBody   string
+	DBReadTime    time.Time
+}
+
+func NewConversationContext(db *sql.DB, senderNumber, messagebody string, isAutoInc bool) *ConversationContext {
+	userInfo, curOrder, userExisted := NewUserInfo(db, senderNumber, isAutoInc)
+	context := &ConversationContext{
+		UserInfo:     userInfo,
+		UserExisted:  userExisted,
+		CurrentOrder: curOrder,
+		MessageBody:  messagebody,
+		DBReadTime:   time.Now(),
+	}
+
+	return context
+}
