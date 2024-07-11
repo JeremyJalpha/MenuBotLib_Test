cb1b8f5260fd474667651184f24d92d6bc7ccd72
JeremyJalpha <jeremyjalpha@gmail.com>
2024-07-11 20:44:09 +0200

    Wired up "constructor"

--- vendor/github.com/JeremyJalpha/MenuBotLib/conversationcontext.go
+++ vendor/github.com/JeremyJalpha/MenuBotLib/conversationcontext.go
@@ -6,20 +6,20 @@ import (
 )
 
 type ConversationContext struct {
-	UserInfo      UserInfo
-	UserExisted   bool
-	PrlstPreamble string
-	PriceList     []CatalogueSelection
-	CurrentOrder  CustomerOrder
-	MessageBody   string
-	DBReadTime    time.Time
+	UserInfo     UserInfo
+	UserExisted  bool
+	PriceList    []CatalogueSelection
+	CurrentOrder CustomerOrder
+	MessageBody  string
+	DBReadTime   time.Time
 }
 
-func NewConversationContext(db *sql.DB, senderNumber, messagebody string, isAutoInc bool) *ConversationContext {
+func NewConversationContext(db *sql.DB, senderNumber, messagebody string, pricelist []CatalogueSelection, isAutoInc bool) *ConversationContext {
 	userInfo, curOrder, userExisted := NewUserInfo(db, senderNumber, isAutoInc)
 	context := &ConversationContext{
 		UserInfo:     userInfo,
 		UserExisted:  userExisted,
+		PriceList:    pricelist,
 		CurrentOrder: curOrder,
 		MessageBody:  messagebody,
 		DBReadTime:   time.Now(),
